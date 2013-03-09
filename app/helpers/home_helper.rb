bf77344dc7b80429b97c736b93eb1dacd82a6189
shannonrdunn <shannonrdunn@gmail.com>
2013-03-09 12:15:21 -0800

    na

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+    def instaconfig
+    Instagram.configure do |config|
+    config.client_id = "efb5bb149cac4d329894cf5ad69ac2be"
+    config.client_secret = "5885b31c06f241a1b059900081406a4c"
+    redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+  end
 end
