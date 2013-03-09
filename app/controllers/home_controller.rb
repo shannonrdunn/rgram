228567c5d3e1353ac2f054a6f5b398a868923ac9
shannonrdunn <shannonrdunn@gmail.com>
2013-03-09 00:37:00 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+require "instagram"
+TAG = "sanfrancisco"
+CALLBACK_URL = "http://ec2-184-72-178-121.compute-1.amazonaws.com:4567/subscribe/callback"
+class HomeController < ApplicationController
+  def index
+	end
+  def insta
+	Instagram.configure do |config|
+  	  config.client_id = "efb5bb149cac4d329894cf5ad69ac2be"
+  	  config.client_secret = "5885b31c06f241a1b059900081406a4c"
+	end
+	Authorize do
+  	  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+	end
+	get "/subscribe/callback" do
+  	  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  	  session[:access_token] = response.access_token
+  	    redirect "/feed"
+	end
+	get "/feed" do
+  	  client = Instagram.client(:access_token => session[:access_token])
+  	  user = client.user
+
+  	  html = "<h1>#{user.username}'s recently liked photos</h1>"
+  	  for media_item in client.tag_recent_media(TAG)
+    	  html << "<img src='#{media_item.images.thumbnail.url}'>"
+  	end
+  html
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Insta::Application.config.secret_token = '91cf8f9e03d5bdc47601ed97d95f30860f68f16e93b76a3a122e2e3b1a3fc3518af609eece01befc0dc0908e6219e6e899ed6e52ec84d3cf786bd1d838dc5e65'
