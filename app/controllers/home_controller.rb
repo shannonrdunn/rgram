6abe5f2be6761ea31bb25c33296b79db4cb05227
shannonrdunn <shannonrdunn@gmail.com>
2013-03-09 12:03:02 -0800

    na

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
     config.client_id = "efb5bb149cac4d329894cf5ad69ac2be"
     config.client_secret = "5885b31c06f241a1b059900081406a4c"
     redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
-    
+  end
    def feed
      client = Instagram.client(:access_token => session[:access_token])
       user = client.user
@@ -22,5 +22,4 @@ class HomeController < ApplicationController
    end
 
 end
-	end
 end
