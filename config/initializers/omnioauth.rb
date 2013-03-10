0a4e24d1609d7f0762e2ce55d48a4aa304e0bf50
shannonrdunn <shannonrdunn@gmail.com>
2013-03-09 17:10:11 -0800

    test

--- config/initializers/omnioauth.rb
+++ config/initializers/omnioauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
+Rails.application.config.middleware.use OmnioAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :instagram, CONFIG['instagram_credentials']['client_id'], CONFIG['instagram_credentials']['client_secret']
 end unless Rails.env.test?
